77158e8fb88e669361e67d5b8348fc7cad112f2b
Steven Forsythia <stvnhd164@gmail.com>
2022-06-23 16:07:26 +0700

    phoenix yearbook

---
+++ script.js
@@ -0,0 +1,80 @@
+// top button scroll
+const goToTop = () => {
+    return location.href="#home"
+}
+
+// card animation scrolling
+function reveal() {
+    var reveals = document.querySelectorAll(".reveal");
+  
+    for (var i = 0; i < reveals.length; i++) {
+      var windowHeight = window.innerHeight;
+      var elementTop = reveals[i].getBoundingClientRect().top;
+      var elementVisible = 150;
+  
+      if (elementTop < windowHeight - elementVisible) {
+        reveals[i].classList.add("active");
+      } else {
+        reveals[i].classList.remove("active");
+      }
+    }
+  }
+
+function reveal2() {
+    var reveals = document.querySelectorAll(".reveal2");
+  
+    for (var i = 0; i < reveals.length; i++) {
+      var windowHeight = window.innerHeight;
+      var elementTop = reveals[i].getBoundingClientRect().top;
+      var elementVisible = 150;
+  
+      if (elementTop < windowHeight - elementVisible) {
+        reveals[i].classList.add("active");
+      } else {
+        reveals[i].classList.remove("active");
+      }
+    }
+  }
+  
+function reveal3() {
+    var reveals = document.querySelectorAll(".reveal3");
+  
+    for (var i = 0; i < reveals.length; i++) {
+      var windowHeight = window.innerHeight;
+      var elementTop = reveals[i].getBoundingClientRect().top;
+      var elementVisible = 150;
+  
+      if (elementTop < windowHeight - elementVisible) {
+        reveals[i].classList.add("active");
+      } else {
+        reveals[i].classList.remove("active");
+      }
+    }
+  }
+
+function reveal4() {
+    var reveals = document.querySelectorAll(".reveal4");
+  
+    for (var i = 0; i < reveals.length; i++) {
+      var windowHeight = window.innerHeight;
+      var elementTop = reveals[i].getBoundingClientRect().top;
+      var elementVisible = 150;
+  
+      if (elementTop < windowHeight - elementVisible) {
+        reveals[i].classList.add("active");
+      } else {
+        reveals[i].classList.remove("active");
+      }
+    }
+  }
+
+  window.addEventListener("scroll", reveal);
+  window.addEventListener("scroll", reveal2);
+  window.addEventListener("scroll", reveal3);
+  window.addEventListener("scroll", reveal4);
+
+  // Swal.fire(
+  //   'PHOENIX SMURAY',
+  //   'MENGGUNAKAN MODE DEKSTOP LEBIH MEMANJAKAN MATA LOH! (＾▽＾)',
+  //   'success'
+  // )
